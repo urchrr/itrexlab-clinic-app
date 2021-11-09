293273f6e413c0b71182f2051aa822dcc437b6df
Ilya Yurchenkov <ilya_su@intekart.ru>
2021-11-09 20:14:11 +0300

    Updates

---
+++ precache-manifest.066c79cf0ba1f6d15a64f6252fb78aa3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8c4ebcc0341ac81613b613dbd91657",
+    "url": "/HW1-itrexlab/index.html"
+  },
+  {
+    "revision": "e695d9667166aebacdc6",
+    "url": "/HW1-itrexlab/static/css/main.ee565540.chunk.css"
+  },
+  {
+    "revision": "c5a1a09c9bfe3d36064c",
+    "url": "/HW1-itrexlab/static/js/2.1ccf3786.chunk.js"
+  },
+  {
+    "revision": "78a30be652cafc19cb4acdb61ab8414b",
+    "url": "/HW1-itrexlab/static/js/2.1ccf3786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822d0e766739aa8d78a3",
+    "url": "/HW1-itrexlab/static/js/3.66c3ad96.chunk.js"
+  },
+  {
+    "revision": "e695d9667166aebacdc6",
+    "url": "/HW1-itrexlab/static/js/main.70f19a59.chunk.js"
+  },
+  {
+    "revision": "7a059bc31749370b30d7",
+    "url": "/HW1-itrexlab/static/js/runtime-main.676503f8.js"
+  },
+  {
+    "revision": "d319a98a33d78cbd3ee4f7966641ef13",
+    "url": "/HW1-itrexlab/static/media/Poppins-Medium.d319a98a.woff2"
+  },
+  {
+    "revision": "ecfdbb20c11be6b0e880067576cc03a9",
+    "url": "/HW1-itrexlab/static/media/Poppins-Medium.ecfdbb20.woff"
+  },
+  {
+    "revision": "26fc2ac807606fd4ea10de435e8ef1e2",
+    "url": "/HW1-itrexlab/static/media/Poppins-Regular.26fc2ac8.woff"
+  },
+  {
+    "revision": "bebb23ead35b42aaf2cb2ddf30d5339b",
+    "url": "/HW1-itrexlab/static/media/Poppins-Regular.bebb23ea.woff2"
+  },
+  {
+    "revision": "135d72377efa35a01edacfdf9ba90172",
+    "url": "/HW1-itrexlab/static/media/Poppins-SemiBold.135d7237.woff"
+  },
+  {
+    "revision": "bb8f49b65a0581c340859912a352f061",
+    "url": "/HW1-itrexlab/static/media/Poppins-SemiBold.bb8f49b6.woff2"
+  },
+  {
+    "revision": "07d6c7199b8eacf3a2e6a6d27865cc61",
+    "url": "/HW1-itrexlab/static/media/bg.07d6c719.png"
+  },
+  {
+    "revision": "af8116e20bffd55d751051d3a768037e",
+    "url": "/HW1-itrexlab/static/media/icon-accept.af8116e2.svg"
+  },
+  {
+    "revision": "f7c650e41035b48d7cbe67b463c34fb8",
+    "url": "/HW1-itrexlab/static/media/icon-block-eye.f7c650e4.svg"
+  },
+  {
+    "revision": "ad7b027facf4064626b4c0714a8e156e",
+    "url": "/HW1-itrexlab/static/media/icon-lock.ad7b027f.svg"
+  },
+  {
+    "revision": "15c53daaaa0b264e610f0c291c61b134",
+    "url": "/HW1-itrexlab/static/media/icon-mail.15c53daa.svg"
+  },
+  {
+    "revision": "2735a298b7c8a91bc49d04a86cc310d6",
+    "url": "/HW1-itrexlab/static/media/icon-person.2735a298.svg"
+  },
+  {
+    "revision": "dd0735d1e58cb4632e3109190d9c1b7f",
+    "url": "/HW1-itrexlab/static/media/icon-schevron-grey.dd0735d1.svg"
+  },
+  {
+    "revision": "4d41c31f2bf64bc6fa1eda986e55289b",
+    "url": "/HW1-itrexlab/static/media/schevron.4d41c31f.svg"
+  }
+]);
